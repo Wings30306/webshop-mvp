fd6d292c424a98801c3af3dce97fc3d74c34c5a6
Joke Heyndels <jo_hannah@outlook.com>
2024-09-20 05:52:13 +0000

    Make shipping address fields optional as not all countries require the same lines

--- cart/models.py
+++ cart/models.py
@@ -3,13 +3,15 @@ from shop.models import ShopItem
 
 # Create your models here.
 class Order(models.Model):
+    """ Order model to save shipping details and payment status """
+    # Relies on Stripe to make sure required fields for each country are filled in
     customer_name = models.CharField(max_length=200) # Do not make unique, we want the same customer to be able to order again
-    delivery_address_line1 = models.CharField(max_length=200)
-    delivery_address_line2 = models.CharField(max_length=200, blank=True, null=True) # Allow to be blank, not all addresses contain this line
-    delivery_address_town = models.CharField(max_length=200)
-    delivery_address_postcode = models.CharField(max_length=20)
-    delivery_address_county = models.CharField(max_length=100, blank=True, null=True) # Allow to be blank, not all countries require this line
-    delivery_address_country = models.CharField(max_length=100)
+    shipping_line1 = models.CharField(max_length=200)
+    shipping_line2 = models.CharField(max_length=200, blank=True, null=True) # Allow to be blank, not all addresses contain this line
+    shipping_town_or_city = models.CharField(max_length=200, blank=True, null=True) # Allow to be blank, not all countries require this line
+    shipping_postal_code = models.CharField(max_length=20, blank=True, null=True) # Allow to be blank, not all countries require this line
+    shipping_county_or_state = models.CharField(max_length=100, blank=True, null=True) # Allow to be blank, not all countries require this line
+    shipping_country = models.CharField(max_length=100)
     order_paid = models.BooleanField(default=False) # On creation, the payment has not gone through yet
 
 class OrderLineItem(models.Model):
